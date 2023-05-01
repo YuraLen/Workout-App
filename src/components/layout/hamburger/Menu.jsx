7269fa2fec55c77616400175113bd1c6e8b10fef
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-05-01 11:52:16 +0300

    update

---
+++ src/components/layout/hamburger/Menu.jsx
@@ -0,0 +1,26 @@
+import cn from "clsx"
+import styles from "./Hamburger.module.scss"
+import { menu } from "./menu.data.js"
+import {Link} from "react-router-dom"
+
+const Menu = ({ isShow }) => {
+	const logoutHandler = () => {
+
+	}
+	return (
+		<nav className={cn(styles.menu, { [styles.show]: isShow })}>
+			<ul>
+				{menu.map((item, index) => (
+					<li key={`_menu_${index}`}>
+						<Link to={item.link}>{item.title}</Link>
+					</li>
+				))}
+				<li>
+					<button onClick={logoutHandler}>Logout</button>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Menu
---
+++ src/components/screens/profile/Profile.jsx
@@ -0,0 +1,9 @@
+import Layout from '../../layout/Layout'
+
+const Profile = () => {
+	return (
+		<Layout>Profile</Layout>
+	)
+}
+
+export default Profile
